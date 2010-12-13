c7f67d1cd56ecd46949f8e91154ca85da3a08757
Steve Grosbois <steve.grosbois@gmail.com>
2010-12-13 06:21:32 -0800

    list is reserved

--- app/controllers/projects_controller.php
+++ app/controllers/projects_controller.php
@@ -2,7 +2,7 @@
 class ProjectsController extends AppController {
     var $name = 'Project';
 
-    function list() {
+    function index() {
         $projects = $this->Project->find('all',
             array('conditions' => array('Project.is_public' => 1)));
         $this->set('projects', $projects);
