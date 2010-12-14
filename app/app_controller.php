3647653aa4705f631ed9e9d7314d2dcd32f5d314
Steve Grosbois <steve.grosbois@gmail.com>
2010-12-14 22:26:45 +0100

    Account

--- app/app_controller.php
+++ app/app_controller.php
@@ -1,4 +1,15 @@
 <?php
 class AppController extends Controller {
 	var $helpers = array('Javascript', 'Html', 'Form');
+	
+	var $uses = array('User');
+	
+	function logged_user($user) {
+		if (!empty($user)) {
+			$this->User->current_user = $user;
+			$this->Session->write('user_id', $user['User']['id']);
+		} else {
+			$this->User->current_user = $this->User->anonymous();
+		}
+	}
 }
--- app/models/User.php
+++ app/models/User.php
@@ -1,4 +1,58 @@
 <?php
 class User extends AppModel {
     var $name = 'User';
+	
+	var $current_user;
+	
+	# Account statuses
+ 	var $STATUS_ANONYMOUS = 0;
+  	var $STATUS_ACTIVE = 1;
+  	var $STATUS_REGISTERED = 2;
+  	var $STATUS_LOCKED = 3;
+	
+	function current() {
+		if (empty($this->$current_user)) {
+			$this->current_user = $this->anonymous();
+		} else if ($anonymous_user['User']['lastname'] != 'Anonymous') {
+			$this->current_user['User']['logged'] = true;
+		}
+		return $this->current_user;
+	}
+	
+  	/**
+  	 * Returns the anonymous user. If the anonymous user does not exist, it is created. There can be only one anonymous user per database.
+  	 */
+  	function anonymous() {
+		$anonymous_user = $this->findByLastname('Anonymous');
+		if (empty($anonymous_user)) {
+			$anonymous_user['User']['lastname'] = 'Anonymous';
+			$anonymous_user['User']['firstname'] = '';
+			$anonymous_user['User']['mail'] = '';
+			$anonymous_user['User']['login'] = '';
+			$anonymous_user['User']['status'] = 0;
+			if(!$this->save($anonymous_user)) {
+				// TODO 'Unable to create the anonymous user.'
+			}
+		}
+		$anonymous_user['User']['logged'] = false;
+		return $anonymous_user;
+	}
+	
+	function logged() {
+		return $this->current_user['User']['logged'];
+	}
+	
+	function try_to_login($login, $password) {
+		if (empty($password)) return null;
+		
+		$user = $this->findByLogin($login);
+		
+		if (!empty($user)) {
+			if ($user['User']['status'] != $STATUS_ACTIVE) return null;
+		}
+	}
+	
+	function hash_password($clear_password) {
+		//TODO return Digest::SHA1.hexdigest(clear_password || "");
+	}
 }
