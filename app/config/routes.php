51e78bf27d317a46613424ffc273361b42e74636
Steve Grosbois <steve.grosbois@gmail.com>
2010-12-14 21:14:49 +0100

    Routes for account

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,2 +1,4 @@
 <?php
     Router::connect('/', array('controller' => 'welcome', 'action' => 'index'));
+    Router::connect('/login', array('controller' => 'account', 'action' => 'login'));
+    Router::connect('/logout', array('controller' => 'account', 'action' => 'logout'));
