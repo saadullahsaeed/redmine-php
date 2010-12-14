6d7b032401790ce77c4bf16465e54ba79cc6a9b8
Steve Grosbois <steve.grosbois@gmail.com>
2010-12-14 08:25:41 -0800

    Balise title

--- app/controllers/projects_controller.php
+++ app/controllers/projects_controller.php
@@ -6,10 +6,14 @@ class ProjectsController extends AppController {
         $projects = $this->Project->find('all',
             array('conditions' => array('Project.is_public' => 1)));
         $this->set('projects', $projects);
+
+        $this->pageTitle = __('Projects', true);
     }
 
     function view($id) {
         $project = $this->Project->findById($id);
         $this->set('project', $project);
+
+        $this->pageTitle = $project['Project']['name'];
     }
 }
