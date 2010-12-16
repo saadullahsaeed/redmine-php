35e81090da06bbaba5fe103dc234aa6f9ba6d511
Steve Grosbois <steve.grosbois@gmail.com>
2010-12-16 01:49:17 -0800

    Traduction des noms de rôle

--- app/models/role.php
+++ app/models/role.php
@@ -2,6 +2,12 @@
 class Role extends AppModel {
     var $name = 'Role';
 
+    var $actsAs = array(
+        'Translate' => array(
+            'name'
+        )
+    );
+
     var $hasMany = array(
         'MemberRole' => array(
             'dependent'=> true
